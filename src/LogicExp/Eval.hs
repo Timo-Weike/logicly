fd34afcb914819d478dcf999d5e89163a5001584
Timo Weike <tweike@techfak.uni-bielefeld.de>
2019-11-07 19:12:58 +0100

    Initial commit of the project

---
+++ src/LogicExp/Eval.hs
@@ -0,0 +1,21 @@
+module LogicExp.Eval
+    (eval)
+where
+
+import LogicExp
+import LogicExp.Valuation
+
+eval :: LogicExp -> Valuation -> Bool
+eval T _            = True
+eval F _            = False
+eval (Lit c) v      = getValue c v 
+eval (Not a) v      = not $ eval a v
+
+eval (And    a b) v = eval a v &&    eval b v
+eval (Or     a b) v = eval a v ||    eval b v
+eval (Xor    a b) v = eval a v `xor` eval b v
+eval (Imp    a b) v = eval a v `imp` eval b v
+eval (Equal  a b) v = eval a v ==    eval b v
+
+xor a b = a /= b
+imp a b = not a || b
---
+++ src/LogicExp/Priority.hs
@@ -0,0 +1,4 @@
+module LogicExp.Priority
+    
+where
+
